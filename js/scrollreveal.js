4e1ba26d14b9a2084bc6e2345961f5c1832e427e
Juri <cerretanijuri@gmail.com>
2021-11-03 16:05:31 +0100

    Added Project

---
+++ js/scrollreveal.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.reveal' , { distance: '80px' , delay: 600 , easing: 'cubic-bezier(0.215, 0.61 , 0.355 , 1)' , interval : 600});
+ScrollReveal().reveal('.reveal-d800' , { distance: '80px' , delay: 800 , easing: 'cubic-bezier(0.215, 0.61 , 0.355 , 1)' , interval : 600});
+ScrollReveal().reveal('.reveal-zoom' , { distance: '80px' , delay: 600 , easing: 'cubic-bezier(0.215, 0.61 , 0.355 , 1)' , interval : 600 , scale: 0.05});
